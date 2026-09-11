7157e833e129183b397e2c6628d562814869170c
Dimas <dimassousa325@gmail.com>
2026-09-11 16:47:34 -0300

    Tela main

---
+++ rota-motors/src/app/interfaces/Icarros.ts
@@ -0,0 +1,17 @@
+interface Icarro{
+    
+    id : string;
+    nome: string;
+    velocidade: number;
+    marca:string;
+    modelo:string;
+    capacidadeTanque:number;
+    manutenção: number;
+    seguroCarro: boolean;
+    potencia: number;
+    anoFabricado:number;
+    preco:number;
+    imagem:string;
+    cor:string;
+
+}
--- rota-motors/src/app/page.tsx
+++ rota-motors/src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 import styles from "./page.module.css"
 import { Search } from "lucide-react";
 import { Play } from 'lucide-react';
-import logo from '../../public/img/logo-sem-fundo.png';
+import { ArrowRight } from 'lucide-react';
+import logo from "./img/logo.png";
+import supra from "./img/carro/supraFundo.png"
 
 
 export default function PaginalInicial() {
@@ -26,30 +28,55 @@ export default function PaginalInicial() {
           <span className={styles.span}><a href="/favoritos">Favoritos</a></span>
           <span className={styles.span}><a href="/sobre">Sobre</a></span>
         </nav>
-        
-          <div className={styles.botoes}>
-            <Search className={styles.lupa} size={22} />
-            <button className={styles.entrar}>Entrar</button>
-            <button className={styles.cadastrar}>Cadastrar</button>
-          </div>
+
+        <div className={styles.botoes}>
+          <Search className={styles.lupa} size={22} />
+          <button className={styles.entrar}>Entrar</button>
+          <button className={styles.cadastrar}>Cadastrar</button>
+        </div>
       </header>
-      <main>
-        <h1>
-          <strong>
-            O SEU DESTINO
-          </strong>
-          <br />
-          <strong style={{ color: "red" }}>
-            PARA CARROS ESPORTIVOS
-          </strong>
-        </h1>
-        <h1 style={{ fontSize: 17 }}>
-          Explore, compare e encontre o carro dos seus sonhos com <br /> a ajuda da nossa Inteligência Artificial.
-        </h1>
-        <button className={styles.explorar}><strong>Explorar Carros</strong></button>
-        
-        <button className={styles.conheca}><strong><Play className={styles.video} size={10}/>Conheça o Rota Motors</strong></button>
-      </main>
+<main className={styles.mano}>
+
+  <video
+    className={styles.videoFundo}
+    autoPlay
+    muted
+    loop
+    playsInline
+  >
+    <source src="/video/carro.mp4" type="video/mp4" />
+  </video>
+
+  <h1 className={styles.linha1}>
+    <strong>
+      O SEU DESTINO
+    </strong>
+    <br />
+    <strong style={{ color: "red" }}>
+      PARA CARROS ESPORTIVOS
+    </strong>
+  </h1>
+
+  <h1 style={{ fontSize: 17, marginLeft: 8 }}>
+    Explore, compare e encontre o carro dos seus sonhos com <br />
+    a ajuda da nossa Inteligência Artificial.
+  </h1>
+
+  <div className={styles.botao}>
+    <button className={styles.explorar}>
+      <strong>Explorar Carros</strong>
+      <ArrowRight size={12} className={styles.seta} />
+    </button>
+
+    <button className={styles.conheca}>
+      <strong>
+        <Play className={styles.video} size={12} />
+        Conheça o Rota Motors
+      </strong>
+    </button>
+  </div>
+
+</main>
       <footer></footer>
     </div>
   );
